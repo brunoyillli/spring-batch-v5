7cfa417820ba01dfa2e957d62d39010163177ff0
brunoyillli <wolfteam1910@gmail.com>
2023-07-13 23:34:07 -0300

    subida projeto

---
+++ src/test/java/io/github/brunoyillli/springbatchv5/SpringBatchV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.springbatchv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
